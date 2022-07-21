18ccd943c31d2e5723a8e3472ce53d84285de7cb
Guillaume Leclerc <guill.bout@gmail.com>
2022-07-21 18:54:37 +0800

    Initial Commit

---
+++ TheInputVentura/TheInputVenturaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheInputVenturaApp.swift
+//  TheInputVentura
+//
+//  Created by Guillaume Leclerc on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheInputVenturaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
